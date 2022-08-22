0008f90c955742322c37322b395862b5fe825cf3
Lloyd Tran (TECHVIFY.G1) <lloyd.tran@techvify.com.vn>
2022-08-22 14:57:01 +0700

    ko co

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
+import { RoutingModule } from './routing.module';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    MovieComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
