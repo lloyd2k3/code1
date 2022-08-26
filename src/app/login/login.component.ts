e4c878d677799cb17a92cdacb5a7161210574f9f
Tluan <tluan1833@GMAIL.COM>
2022-08-26 14:00:50 +0700

    +1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Routes } from '@angular/router';
 import { routes } from '../routing.module';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,19 +13,19 @@ export class LoginComponent implements OnInit {
   password = "";
   errorMsg = "";  
 
-constructor( private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-
   }
-  login(){
+
+  login() {
     if (this.username.trim().length === 0) {
       this.errorMsg = "Username is required";
     } else if (this.password.trim().length === 0) {
       this.errorMsg = "Password is required";
     } else {
       this.errorMsg = "";
-      let res = 200;
+      let res = this.auth.login(this.username, this.password);
       if (res === 200) {
         this.router.navigate(['home']);
       }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+login(uname: string, pword: string) {
+  if (uname === 'suren' && pword === '1234') {
+    return 200;
+  } else {
+    return 403;
+  }
+}}
+
